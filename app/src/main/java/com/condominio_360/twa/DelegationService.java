940efebd600cae7619a9c9399869873969a8751a
Jonathan Melendez <soyjonnymelendez@gmail.com>
2026-04-14 12:19:32 -0400

    ignore

---
+++ app/src/main/java/com/condominio_360/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.condominio_360.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
